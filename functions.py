098a89e0237a1af3ed3c2cd33ed9f12f755ebde0
Favour Nwajoko <chocorazzee@gmail.com>
2022-07-09 23:58:13 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -4,3 +4,12 @@ def initial_parameters():
     second_weights = np.random.rand(10, 10) - .5
     second_bias = np.random.rand(10, 1) - .5
     return first_weights, first_bias, second_weights, second_bias
+
+def Leaky_ReLU(step_1, alpha):
+    for sub_array in step_1:
+        for i, value in enumerate(sub_array):
+            if value>0:
+                sub_array[i] = value
+            else:
+                sub_array[i] = (alpha*value)
+    return step_1
